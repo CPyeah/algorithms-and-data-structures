b26d270c306bd1fd78d9a08a8887b745068ae2a7
chengpeng <chengpengyeah@126.com>
2021-12-24 14:25:22 +0800

    最大子数组和

--- src/main/java/divide_and_conquer/MaxSubArray.java
+++ src/main/java/divide_and_conquer/MaxSubArray.java
@@ -80,7 +80,7 @@ public class MaxSubArray {
 			dp[0] = nums[0];
 			int max = dp[0];
 			for (int i = 1; i < nums.length; i++) {
-				int m = max(nums[i], dp[i - 1] + nums[i]);
+				int m = Math.max(nums[i], dp[i - 1] + nums[i]);
 				dp[i] = m;
 				if (m > max) {
 					max = m;
