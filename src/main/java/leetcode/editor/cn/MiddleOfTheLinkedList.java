c763596687d0979e6e8aa36c7b062ebb2b6ecc7b
chengpeng <chengpengyeah@126.com>
2022-03-09 17:40:21 +0800

    [876] 链表的中间结点

---
+++ src/main/java/leetcode/editor/cn/MiddleOfTheLinkedList.java
@@ -0,0 +1,34 @@
+  package leetcode.editor.cn;
+
+  import model.ListNode;
+
+  /**
+   * [876] 链表的中间结点
+   */
+  public class MiddleOfTheLinkedList{
+
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
