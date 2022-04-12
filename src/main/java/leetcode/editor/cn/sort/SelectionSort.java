7b651ac61319115e2fbdc2b14a55eafe5c678a22
chengpeng <chengpengyeah@126.com>
2022-04-12 10:51:17 +0800

    选择排序

--- src/main/java/leetcode/editor/cn/sort/SelectionSort.java
+++ src/main/java/leetcode/editor/cn/sort/SelectionSort.java
@@ -1,12 +1,23 @@
 package leetcode.editor.cn.sort;
 
-
+/**
+ * 选择排序， 每次循环找出最小值，往前面换
+ */
 public class SelectionSort {
 
 	public static class Solution {
 
 		public int[] sort(int[] nums) {
-
+			int minIndex;
+			for (int i = 0; i < nums.length - 1; i++) {
+				minIndex = i;
+				for (int j = i; j < nums.length; j++) {
+					if (nums[j] < nums[minIndex]) {
+						minIndex = j;
+					}
+				}
+				SortUtil.swap(nums, i, minIndex);
+			}
 			return nums;
 		}
 	}
