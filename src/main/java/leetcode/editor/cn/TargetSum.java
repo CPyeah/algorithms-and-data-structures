5770f761de19db970f20e004cdaa6a9aa2fb2796
chengpeng <chengpengyeah@126.com>
2022-09-06 09:50:19 +0800

    [494] 目标和 动态规划 map优化

--- src/main/java/leetcode/editor/cn/TargetSum.java
+++ src/main/java/leetcode/editor/cn/TargetSum.java
@@ -1,5 +1,8 @@
 package leetcode.editor.cn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * [494] 目标和
  */
@@ -7,30 +10,31 @@ public class TargetSum {
 
 	public static void main(String[] args) {
 		Solution solution = new TargetSum().new Solution();
+		solution.findTargetSumWays(new int[]{1}, 2);
 	}
 
 	//leetcode submit region begin(Prohibit modification and deletion)
 	class Solution {
 
 		public int findTargetSumWays(int[] nums, int target) {
-			int[][] dp = new int[nums.length][2002];
-			dp[0][1001 + nums[0]] += 1;
-			dp[0][1001 - nums[0]] += 1;
-			// dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j+nums[i]]
-			for (int i = 1; i < dp.length; i++) {
-				for (int j = 0; j < dp[0].length; j++) {
-					dp[i][j] = getFromDP(dp, i-1, j-nums[i]) + getFromDP(dp, i-1, j+nums[i]);
-				}
+			Map<Integer, Integer> dp =new HashMap<>();
+			dp.put(nums[0], dp.getOrDefault(nums[0], 0)+1);
+			dp.put(-nums[0], dp.getOrDefault(-nums[0], 0)+1);
+			for (int i = 1; i < nums.length; i++) {
+				Map<Integer, Integer> newDP =new HashMap<>();
+				int n = nums[i];
+				dp.forEach((k, v) -> {
+					int leftKey = k - n;
+					newDP.put(leftKey, newDP.getOrDefault(leftKey, 0) + v);
+					int rightKey = k + n;
+					newDP.put(rightKey, newDP.getOrDefault(rightKey, 0) + v);
+				});
+				dp = newDP;
 			}
-			return dp[dp.length-1][1001+target];
+			return dp.getOrDefault(target, 0);
 		}
 
-		private int getFromDP(int[][] dp, int i, int j) {
-			if(j<0 || j>dp[0].length-1) {
-				return 0;
-			}
-			return dp[i][j];
-		}
+
 	}
 //leetcode submit region end(Prohibit modification and deletion)
 
