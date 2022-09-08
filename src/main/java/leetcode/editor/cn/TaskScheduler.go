36678beb831ffc64a05c271ad90d1baac08652a0
chengpeng <chengpengyeah@126.com>
2022-09-08 14:01:20 +0800

    [621] 任务调度器

--- src/main/java/leetcode/editor/cn/TaskScheduler.go
+++ src/main/java/leetcode/editor/cn/TaskScheduler.go
@@ -9,7 +9,27 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func leastInterval(tasks []byte, n int) int {
-
+  digits := make([]int, 26)
+  for _,v := range tasks {
+    digits[v-'A'] ++
+  }
+  sort.Slice(digits, func(i int, j int)bool{
+    return digits[i] > digits[j]
+  })
+  maxCount := digits[0]
+  maxItems := 0
+  for _,v := range digits {
+    if v == maxCount {
+      maxItems ++
+    } else {
+      break
+    }
+  }
+  ans := (maxCount - 1) * (n + 1) + maxItems
+  if len(tasks) > ans {
+    ans = len(tasks)
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
