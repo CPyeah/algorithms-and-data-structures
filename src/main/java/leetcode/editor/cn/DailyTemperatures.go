b3b8c518f6c00951d7ce5b9d7cf1037510453b48
chengpeng <chengpengyeah@126.com>
2022-09-08 14:45:45 +0800

    [739] 每日温度 单调递减栈

--- src/main/java/leetcode/editor/cn/DailyTemperatures.go
+++ src/main/java/leetcode/editor/cn/DailyTemperatures.go
@@ -2,6 +2,7 @@
   
   /**
    * [739] 每日温度
+   [73,74,75,71,69,72,76,73]
    */
   public class DailyTemperatures{
       public static void main(String[] args) {
@@ -9,7 +10,20 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func dailyTemperatures(temperatures []int) []int {
+  ans := make([]int, len(temperatures))
+  stack := make([]int, 0)
+  for i:=0;i<len(temperatures);i++ {
 
+    for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]]{
+      // pop
+      topIndex := stack[len(stack)-1]
+      stack = stack[:len(stack)-1]
+      ans[topIndex] = i - topIndex
+    }
+    stack = append(stack, i)
+
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
