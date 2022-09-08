0986d9c1544e7f4492bf25e118f7bc646d75e7e4
chengpeng <chengpengyeah@126.com>
2022-09-08 14:19:53 +0800

    [647] 回文子串

--- src/main/java/leetcode/editor/cn/PalindromicSubstrings.go
+++ src/main/java/leetcode/editor/cn/PalindromicSubstrings.go
@@ -9,7 +9,23 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func countSubstrings(s string) int {
+  ans = 0
+  for i:=0;i<len(s);i++ {
+    checkTheSlides(s, i, i)
+    checkTheSlides(s, i, i+1)
+  }
+  return ans
+}
+var ans int
 
+func checkTheSlides(s string, i int, j int) {
+  if i < 0 || j > len(s) - 1 {
+    return
+  }
+  if s[i] == s[j] {
+    ans ++
+    checkTheSlides(s, i-1, j+1)
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
