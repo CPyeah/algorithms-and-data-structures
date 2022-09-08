4f81f0426d378f8e0c8fb16f370b9ee3c8f9769a
chengpeng <chengpengyeah@126.com>
2022-09-08 10:58:58 +0800

    extend file

---
+++ src/main/java/leetcode/editor/cn/AddTwoNumbers.go
@@ -0,0 +1,23 @@
+  package leetcode.editor.cn;
+  
+  /**
+   * [2] 两数相加
+   */
+  public class AddTwoNumbers{
+      public static void main(String[] args) {
+           Solution solution = new AddTwoNumbers().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
