d536a8ce5c9f9a72007cecae48f45b3f1fbce0a6
chengpeng <chengpengyeah@126.com>
2022-09-12 13:12:52 +0800

    [11] 盛最多水的容器

--- src/main/java/leetcode/editor/cn/ContainerWithMostWater.go
+++ src/main/java/leetcode/editor/cn/ContainerWithMostWater.go
@@ -9,7 +9,35 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func maxArea(height []int) int {
+  l := 0
+  r := len(height)-1
+  ans := 0
+  for l < r {
+    // calculate area
+    area := (r - l) * min(height[l], height[r])
+    ans = max(ans, area)
+    // select and move pointer
+    if height[l] < height[r] {
+      l ++
+    } else {
+      r --
+    }
+  }
+  return ans
+}
 
+func min(a int, b int) int {
+  if a < b {
+    return a
+  }
+  return b
+}
+
+func max(a int, b int) int {
+  if a > b {
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
