dde423e64a9cfc964d2b50649757504c5bacb2eb
chengpeng <chengpengyeah@126.com>
2022-09-13 16:41:17 +0800

    [20] 有效的括号

--- src/main/java/leetcode/editor/cn/ValidParentheses.go
+++ src/main/java/leetcode/editor/cn/ValidParentheses.go
@@ -9,7 +9,31 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func isValid(s string) bool {
-
+  dict := make(map[byte]byte)
+  dict[')'] = '('
+  dict[']'] = '['
+  dict['}'] = '{'
+  stack := make([]byte, 0)
+  for i:=0;i<len(s);i++ {
+    // if current is left
+    if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+      // push to stack
+      stack = append(stack, s[i])
+    } else if len(stack) == 0 {
+      return false
+    } else {
+      // if current is right
+      // check stack top if is left
+      top := stack[len(stack)-1]
+      if top != dict[s[i]] {
+        return false
+      } else {
+        // pop
+        stack = stack[:len(stack)-1]
+      }
+    }
+  }
+  return len(stack) == 0
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
