15f6c3df73a3134fdfb538736c497f5463fd725d
chengpeng <chengpengyeah@126.com>
2022-09-13 20:44:44 +0800

    [23] 合并K个升序链表

--- src/main/java/leetcode/editor/cn/MergeKSortedLists.go
+++ src/main/java/leetcode/editor/cn/MergeKSortedLists.go
@@ -16,7 +16,34 @@
  * }
  */
 func mergeKLists(lists []*ListNode) *ListNode {
+  dummy := &ListNode{}
+  p := dummy
+  for {
+    nextNode := getNextNode(lists)
+    if nextNode == nil {
+      break
+    }
+    p.Next = nextNode
+    p = p.Next
+  }
+  return dummy.Next
+}
 
+func getNextNode(lists []*ListNode) *ListNode {
+  minVal := 10001
+  var ret *ListNode = nil
+  index := -1
+  for i:=0;i<len(lists);i++ {
+    if lists[i] != nil && lists[i].Val < minVal{
+      minVal = lists[i].Val
+      ret = lists[i]
+      index = i
+    }
+  }
+  if index > -1 {
+    lists[index] = lists[index].Next
+  }
+  return ret
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
