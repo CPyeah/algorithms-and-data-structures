6bda948a7d4b0c20479bb07f2cf70474d762657d
chengpeng <chengpengyeah@126.com>
2022-09-14 11:29:17 +0800

    [32] 最长有效括号

--- src/main/java/leetcode/editor/cn/LongestValidParentheses.go
+++ src/main/java/leetcode/editor/cn/LongestValidParentheses.go
@@ -9,7 +9,35 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func longestValidParentheses(s string) int {
-
+  stack:=[]int{}
+  // string go through stack
+  for i:=0;i<len(s);i++ {
+    if len(stack) == 0 {
+      stack = append(stack, i)
+    } else if s[i] == '(' {
+      stack = append(stack, i)
+    } else {
+      topChar := s[stack[len(stack)-1]]
+      if '(' == topChar {
+        // pop
+        stack = stack[:len(stack)-1]
+      } else {
+        stack = append(stack, i)
+      }
+    }
+  }
+  // add head and tail
+  stack = append([]int{-1}, stack...)
+  stack = append(stack, len(s))
+  // get max interval
+  max := 0
+  for i:=1;i<len(stack);i++ {
+    interval := stack[i] - stack[i-1] -1
+    if interval > max {
+      max = interval
+    }
+  }
+  return max
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
