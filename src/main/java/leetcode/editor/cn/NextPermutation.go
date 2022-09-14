f6ffb481c1c133ff151e6a1f32fc836f6fe4aa6b
chengpeng <chengpengyeah@126.com>
2022-09-14 10:48:23 +0800

    [31] 下一个排列

--- src/main/java/leetcode/editor/cn/NextPermutation.go
+++ src/main/java/leetcode/editor/cn/NextPermutation.go
@@ -9,7 +9,37 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func nextPermutation(nums []int)  {
+  if len(nums) < 2 {
+    return
+  }
+  // find smaller than next place
+  var targetIndex int
+  for targetIndex=len(nums)-2;targetIndex>=0;targetIndex-- {
+    if nums[targetIndex] < nums[targetIndex+1] {
+      break
+    }
+  }
+  if targetIndex == -1 {
+    reverse(nums, 0, len(nums)-1)
+  } else {
+    // find just bigger than target item and swap
+    for i := len(nums)-1; i > targetIndex; i-- {
+      if nums[i] > nums[targetIndex] {
+        nums[i], nums[targetIndex] = nums[targetIndex], nums[i]
+        break
+      }
+    }
+    // reverse targetIndex right items
+    reverse(nums, targetIndex+1, len(nums)-1)
+  }
+}
 
+func reverse(nums []int, l int, r int) {
+  for l < r {
+    nums[l], nums[r] = nums[r], nums[l]
+    l++
+    r--
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
---
+++ src/main/java/leetcode/editor/cn/SwapNodesInPairs.go
@@ -0,0 +1,23 @@
+  package leetcode.editor.cn;
+  
+  /**
+   * [24] 两两交换链表中的节点
+   */
+  public class SwapNodesInPairs{
+      public static void main(String[] args) {
+           Solution solution = new SwapNodesInPairs().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
