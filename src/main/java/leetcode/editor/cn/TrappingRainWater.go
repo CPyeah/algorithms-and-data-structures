94372b0e8678ae6deb377198aaa0f3cbd0182f44
chengpeng <chengpengyeah@126.com>
2022-09-15 10:30:58 +0800

    [42] 接雨水

--- src/main/java/leetcode/editor/cn/TrappingRainWater.go
+++ src/main/java/leetcode/editor/cn/TrappingRainWater.go
@@ -9,7 +9,32 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func trap(height []int) int {
+  stack := []int{}
+  total := 0
+  for i:=0;i<len(height);i++ {
+    for len(stack) > 0 && height[i] > height[stack[len(stack)-1]] {
+      // pop
+      bottomIndex := stack[len(stack)-1]
+      stack = stack[:len(stack)-1]
+      if len(stack) == 0 {
+        break
+      }
+      // peek
+      leftIndex := stack[len(stack)-1]
 
+      v := (i - leftIndex - 1) * (min(height[i], height[leftIndex]) - height[bottomIndex])
+      total += v
+    }
+    stack = append(stack, i)
+  }
+  return total
+}
+
+func min(a int, b int) int {
+  if a < b {
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
