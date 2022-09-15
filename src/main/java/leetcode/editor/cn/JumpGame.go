2ca90b84e32ac296151caa52842066b1f192892b
chengpeng <chengpengyeah@126.com>
2022-09-15 23:23:04 +0800

    [55] 跳跃游戏

--- src/main/java/leetcode/editor/cn/JumpGame.go
+++ src/main/java/leetcode/editor/cn/JumpGame.go
@@ -9,7 +9,24 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func canJump(nums []int) bool {
+  if len(nums) < 2 {
+    return true
+  }
+  maxDistant := nums[0]
+  for i:=1;i<=maxDistant;i++ {
+    if maxDistant >= len(nums)-1 {
+      return true
+    }
+    maxDistant = max(maxDistant, i+nums[i])
+  }
+  return false
+}
 
+func max(a int, b int)int {
+  if a>b {
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
