fb40cac1fc90c7d409b2ae683af07d24346357d6
chengpeng <chengpengyeah@126.com>
2022-09-15 14:39:01 +0800

    [53] 最大子数组和

--- src/main/java/leetcode/editor/cn/MaximumSubarray.go
+++ src/main/java/leetcode/editor/cn/MaximumSubarray.go
@@ -9,7 +9,21 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func maxSubArray(nums []int) int {
+  dp := make([]int, len(nums))
+  dp[0] = nums[0]
+  ans := dp[0]
+  for i:=1;i<len(nums);i++ {
+    dp[i] = max(nums[i], dp[i-1] + nums[i])
+    ans = max(ans, dp[i])
+  }
+  return ans
+}
 
+func max(a int, b int)int {
+  if a > b {
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
