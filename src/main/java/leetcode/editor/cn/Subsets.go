f44426faa5a07443b96aa098178081c6810d0c33
chengpeng <chengpengyeah@126.com>
2022-09-16 14:20:53 +0800

    [78] 子集

--- src/main/java/leetcode/editor/cn/Subsets.go
+++ src/main/java/leetcode/editor/cn/Subsets.go
@@ -9,7 +9,21 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func subsets(nums []int) [][]int {
-
+  ans := make([][]int, 0)
+  ans = append(ans, []int{})
+  for _, n := range nums {
+    currentLen := len(ans)
+    for i:=0;i<currentLen;i++ {
+      inAns := ans[i]
+      newAns := make([]int, len(inAns))
+      copy(newAns, inAns)
+      newAns = append(newAns, n)
+      // 诡，居然需要排序才能通过
+      sort.Slice(newAns, func(i int, j int)bool{return newAns[i]<newAns[j]})
+      ans = append(ans, newAns)
+    }
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
