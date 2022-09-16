37020ec206e810ed0b7ffb6837b9a89de8987c69
chengpeng <chengpengyeah@126.com>
2022-09-16 15:14:11 +0800

    [76] 最小覆盖子串

--- src/main/java/leetcode/editor/cn/MinimumWindowSubstring.go
+++ src/main/java/leetcode/editor/cn/MinimumWindowSubstring.go
@@ -9,7 +9,64 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func minWindow(s string, t string) string {
+  dict = make(map[byte]int)
+  // 初始化字典
+  initDict(t)
 
+  l := 0
+  r := 0
+  add(s[0])
+  minSize := len(s) + 1
+  res := ""
+
+  for r < len(s) {
+    if contains() {
+      if r - l < minSize {
+        res = s[l:r+1]
+        minSize = r - l
+      }
+      remove(s[l])
+      l++
+    } else {
+      r ++
+      if r < len(s) {
+        add(s[r])
+      }
+    }
+  }
+
+  return res
+}
+
+var dict map[byte]int
+
+func initDict(t string) {
+  for i := range t {
+    dict[t[i]] ++
+  }
+}
+
+func add(b byte) {
+  _, ok := dict[b]
+  if ok {
+    dict[b] --
+  }
+}
+
+func remove(b byte) {
+    _, ok := dict[b]
+    if ok {
+      dict[b] ++
+    }
+}
+
+func contains() bool {
+  for _,v := range dict {
+    if v > 0 {
+      return false
+    }
+  }
+  return true
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
