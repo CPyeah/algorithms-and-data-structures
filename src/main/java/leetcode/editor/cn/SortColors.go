4440e56e58c0d82db11ece8b9ac54e9d3938191a
chengpeng <chengpengyeah@126.com>
2022-09-16 13:53:16 +0800

    [75] 颜色分类

--- src/main/java/leetcode/editor/cn/SortColors.go
+++ src/main/java/leetcode/editor/cn/SortColors.go
@@ -9,7 +9,27 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func sortColors(nums []int)  {
-
+  p0 := 0
+  p2 := len(nums)-1
+  i := 0
+  for i <= p2 {
+    if nums[i] == 0 {
+      swap(nums, p0, i)
+      i ++
+      p0 ++
+    } else if nums[i] == 2 {
+      swap(nums, p2, i)
+      p2 --
+    } else {
+      i ++
+    }
+  }
+}
+func swap(nums []int, i int, j int) {
+  if i == j {
+    return
+  }
+  nums[i], nums[j] = nums[j], nums[i]
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
