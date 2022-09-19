22d8458ba70d37039c0c44b636ea37ae72e54035
chengpeng <chengpengyeah@126.com>
2022-09-19 10:40:45 +0800

    [84] 柱状图中最大的矩形

--- src/main/java/leetcode/editor/cn/LargestRectangleInHistogram.go
+++ src/main/java/leetcode/editor/cn/LargestRectangleInHistogram.go
@@ -9,7 +9,32 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func largestRectangleArea(heights []int) int {
+  heights = append(heights, 0)
+  heights = append([]int{0}, heights...)
+  stack := make([]int, 0)
+  maxArea := 0
+  for i:=0;i<len(heights);i++ {
+    for len(stack) > 0 && heights[i] < heights[stack[len(stack)-1]] {
+      // pop
+      topIndex := stack[len(stack)-1]
+      stack = stack[:len(stack)-1]
 
+      // area = pop() * (i - peek() - 1)
+      leftSideIndex := stack[len(stack)-1]
+      currentArea := (i - leftSideIndex - 1) * heights[topIndex]
+      maxArea = max(maxArea, currentArea)
+    }
+    // push
+    stack = append(stack, i)
+  }
+
+  return maxArea
+}
+func max(a int, b int) int {
+  if a > b {
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
