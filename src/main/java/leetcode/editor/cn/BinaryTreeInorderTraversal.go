756b3ef5e55acb65f0bbc378f936c5b9870bd9a2
chengpeng <chengpengyeah@126.com>
2022-09-19 13:59:09 +0800

    [94] 二叉树的中序遍历

--- src/main/java/leetcode/editor/cn/BinaryTreeInorderTraversal.go
+++ src/main/java/leetcode/editor/cn/BinaryTreeInorderTraversal.go
@@ -17,7 +17,18 @@
  * }
  */
 func inorderTraversal(root *TreeNode) []int {
-
+  ans := make([]int, 0)
+  var dfs func(root *TreeNode)
+  dfs = func(root *TreeNode) {
+    if root == nil {
+      return
+    }
+    dfs(root.Left)
+    ans = append(ans, root.Val)
+    dfs(root.Right)
+  }
+  dfs(root)
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
