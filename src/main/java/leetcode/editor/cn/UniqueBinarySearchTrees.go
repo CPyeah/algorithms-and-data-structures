54d133d062cabc7758d7a93d3866942d7fe640d0
chengpeng <chengpengyeah@126.com>
2022-09-19 14:19:45 +0800

    [96] 不同的二叉搜索树

--- src/main/java/leetcode/editor/cn/UniqueBinarySearchTrees.go
+++ src/main/java/leetcode/editor/cn/UniqueBinarySearchTrees.go
@@ -9,7 +9,17 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func numTrees(n int) int {
-
+  dp := make([]int, n+1)
+  dp[0] = 1
+  for i:=1;i<=n;i++ {
+    subNodeCount := i-1
+    sum := 0
+    for j:=0;j<=subNodeCount;j++ {
+      sum += dp[j] * dp[subNodeCount-j]
+    }
+    dp[i] = sum
+  }
+  return dp[n]
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
