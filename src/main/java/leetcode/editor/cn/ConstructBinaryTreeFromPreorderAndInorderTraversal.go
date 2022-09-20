82ea27811274cb86489aeee7483ff94b356e3d86
chengpeng <chengpengyeah@126.com>
2022-09-20 11:19:49 +0800

    [105] 从前序与中序遍历序列构造二叉树

--- src/main/java/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.go
+++ src/main/java/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.go
@@ -16,8 +16,26 @@
  *     Right *TreeNode
  * }
  */
+ // preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
 func buildTree(preorder []int, inorder []int) *TreeNode {
+  if len(preorder) == 0 {
+    return nil
+  }
+  rootVal := preorder[0] // 3
+  rootNode := &TreeNode{Val:rootVal}
+  var rootIndexInInorder int // 1
+  for i:=0;i<len(inorder);i++ {
+    if inorder[i] == rootVal {
+      rootIndexInInorder = i
+      break
+    }
+  }
+  // [9] , [9]
+  rootNode.Left = buildTree(preorder[1:rootIndexInInorder+1], inorder[:rootIndexInInorder])
+  // [20, 15, 7], [15, 20, 7]
+  rootNode.Right = buildTree(preorder[rootIndexInInorder+1:], inorder[rootIndexInInorder+1:])
 
+  return rootNode
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
--- src/main/java/leetcode/editor/cn/doc/note/ConstructBinaryTreeFromPreorderAndInorderTraversal.md
+++ src/main/java/leetcode/editor/cn/doc/note/ConstructBinaryTreeFromPreorderAndInorderTraversal.md
@@ -1,2 +1,3 @@
 preorder 里面找root
 inorder 里面找leftSub和 rightSub
+递归
