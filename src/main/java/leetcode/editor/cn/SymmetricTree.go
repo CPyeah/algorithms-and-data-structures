3ab1da220add659b808cb2991d66b84295938571
chengpeng <chengpengyeah@126.com>
2022-09-20 10:31:47 +0800

    [101] 对称二叉树

--- src/main/java/leetcode/editor/cn/SymmetricTree.go
+++ src/main/java/leetcode/editor/cn/SymmetricTree.go
@@ -17,7 +17,21 @@
  * }
  */
 func isSymmetric(root *TreeNode) bool {
+  return dfs(root.Left, root.Right)
+}
 
+// 已 2 为参考
+func dfs(node1 *TreeNode, node2 *TreeNode) bool {
+  if node1 == nil && node2 == nil {
+    return true
+  }
+  if node1 == nil || node2 == nil {
+    return false
+  }
+  if node1.Val != node2.Val {
+    return false
+  }
+  return dfs(node1.Left, node2.Right) && dfs(node1.Right, node2.Left)
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
