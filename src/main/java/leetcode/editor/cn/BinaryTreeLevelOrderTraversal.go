ce53b710a8a522e85bfe82aa08fdc04403f5215a
chengpeng <chengpengyeah@126.com>
2022-09-20 10:25:58 +0800

    [102] 二叉树的层序遍历

--- src/main/java/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.go
+++ src/main/java/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.go
@@ -17,7 +17,30 @@
  * }
  */
 func levelOrder(root *TreeNode) [][]int {
-
+  ans := make([][]int, 0)
+  if root == nil {
+    return ans
+  }
+  queue := make([]*TreeNode, 0)
+  queue = append(queue, root)
+  for len(queue) > 0 {
+    // fix the size
+    currentLevelSize := len(queue)
+    res := make([]int, currentLevelSize)
+    for i:=0;i<currentLevelSize;i++ {
+      res[i] = queue[i].Val
+      // add sub nodes in queue
+      if queue[i].Left != nil {
+        queue = append(queue, queue[i].Left)
+      }
+      if queue[i].Right != nil {
+        queue = append(queue, queue[i].Right)
+      }
+    }
+    ans = append(ans, res)
+    queue = queue[currentLevelSize:]
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
