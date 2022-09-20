8a6438f1621601224e5610f1e2423edbf3ca3c44
chengpeng <chengpengyeah@126.com>
2022-09-20 14:31:30 +0800

    [124] 二叉树中的最大路径和

--- src/main/java/leetcode/editor/cn/BinaryTreeMaximumPathSum.go
+++ src/main/java/leetcode/editor/cn/BinaryTreeMaximumPathSum.go
@@ -17,7 +17,35 @@
  * }
  */
 func maxPathSum(root *TreeNode) int {
-
+  maxSum := root.Val
+  var dfs func(node *TreeNode) int
+  dfs = func(node *TreeNode) int {
+    if node == nil {
+      return 0
+    }
+    rootVal := node.Val
+    leftMax := dfs(node.Left)
+    rightMax := dfs(node.Right)
+    rootWithLeft := rootVal + leftMax
+    rootWithRight := rootVal + rightMax
+    rootWithBothSide := rootVal + leftMax + rightMax
+    // 根、 左根、 右根 可以继续向上链接， 取最大值返回
+    retVal := max(rootVal, rootWithLeft, rootWithRight)
+    // 左根右   无法继续向上链接，判断当前最大值
+    maxSum = max(maxSum, retVal, rootWithBothSide)
+    return retVal
+  }
+  dfs(root)
+  return maxSum
+}
+func max(nums ...int) int {
+  m := nums[0]
+  for i:=1;i<len(nums);i++ {
+    if nums[i] > m {
+      m = nums[i]
+    }
+  }
+  return m
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
