2c478c6aebc5c3acda3863285950a169e70105a6
chengpeng <chengpengyeah@126.com>
2022-09-21 14:10:29 +0800

    [128] 最长连续序列

--- src/main/java/leetcode/editor/cn/LongestConsecutiveSequence.go
+++ src/main/java/leetcode/editor/cn/LongestConsecutiveSequence.go
@@ -9,7 +9,24 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func longestConsecutive(nums []int) int {
-
+  dict := make(map[int]bool)
+  for _, n := range nums {
+    dict[n] = true
+  }
+  ans := 0
+  for k,_ := range dict {
+    if dict[k-1] {
+      continue
+    }
+    count := 1
+    for dict[k+count] {
+      count ++
+    }
+    if count > ans {
+      ans = count
+    }
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
