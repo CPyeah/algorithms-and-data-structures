8c89ed32beff4c81c4d72215543cc25cf7578029
chengpeng <chengpengyeah@126.com>
2022-09-21 15:22:57 +0800

    [142] 环形链表 II

--- src/main/java/leetcode/editor/cn/LinkedListCycleIi.go
+++ src/main/java/leetcode/editor/cn/LinkedListCycleIi.go
@@ -16,7 +16,21 @@
  * }
  */
 func detectCycle(head *ListNode) *ListNode {
-    
+    fast := head
+    slow := head
+    for fast != nil && fast.Next != nil {
+      fast = fast.Next.Next
+      slow = slow.Next
+      if slow == fast {
+        fast = head
+        for slow != fast {
+          slow = slow.Next
+          fast = fast.Next
+        }
+        return slow
+      }
+    }
+    return nil
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
