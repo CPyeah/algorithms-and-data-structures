c94b16a6464644f786464cd7f3f76786e7569c0f
chengpeng <chengpengyeah@126.com>
2022-09-21 14:13:50 +0800

    [136] 只出现一次的数字

--- src/main/java/leetcode/editor/cn/SingleNumber.go
+++ src/main/java/leetcode/editor/cn/SingleNumber.go
@@ -9,7 +9,14 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func singleNumber(nums []int) int {
-
+  if len(nums) == 0 {
+    return 0
+  }
+  ans := nums[0]
+  for i:=1;i<len(nums);i++ {
+    ans ^= nums[i]
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
