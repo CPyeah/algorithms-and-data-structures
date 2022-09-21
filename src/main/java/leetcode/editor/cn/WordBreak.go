4f526d5d3578cd099bcd0e35d023ac0eb2965eaa
chengpeng <chengpengyeah@126.com>
2022-09-21 15:49:48 +0800

    [139] 单词拆分

--- src/main/java/leetcode/editor/cn/WordBreak.go
+++ src/main/java/leetcode/editor/cn/WordBreak.go
@@ -9,7 +9,23 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func wordBreak(s string, wordDict []string) bool {
+  dict := make(map[string]bool)
+  for _,w := range wordDict {
+    dict[w] = true
+  }
 
+  dp := make([]int, 0)
+  dp = append(dp, 0)
+  for i:=0;i<len(s);i++ {
+    for _, d := range dp {
+      sub := s[d:i+1]
+      if dict[sub] {
+        dp = append(dp, i+1)
+        break
+      }
+    }
+  }
+  return dp[len(dp) -1] == len(s)
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
