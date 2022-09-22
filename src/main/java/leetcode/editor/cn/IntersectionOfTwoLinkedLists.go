88d0b6bfb638b8ff13138c0767b6573d28cb50ea
chengpeng <chengpengyeah@126.com>
2022-09-22 11:16:53 +0800

    [160] 相交链表

--- src/main/java/leetcode/editor/cn/IntersectionOfTwoLinkedLists.go
+++ src/main/java/leetcode/editor/cn/IntersectionOfTwoLinkedLists.go
@@ -16,7 +16,21 @@
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-    
+    pA := headA
+    pB := headB
+    for pA != pB{
+      if pA == nil {
+        pA = headB
+      } else {
+        pA = pA.Next
+      }
+      if pB == nil {
+        pB = headA
+      } else {
+        pB = pB.Next
+      }
+    }
+    return pA
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
