cb6b3a2d901e178f2a88526a663333ac7e5b3113
chengpeng <chengpengyeah@126.com>
2022-09-22 14:04:06 +0800

    [169] 多数元素

--- src/main/java/leetcode/editor/cn/MajorityElement.go
+++ src/main/java/leetcode/editor/cn/MajorityElement.go
@@ -9,7 +9,21 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func majorityElement(nums []int) int {
-
+  current := nums[0]
+  count := 1
+  for i:=1;i<len(nums);i++ {
+    if count == 0 {
+      current = nums[i]
+      count = 1
+    } else {
+      if current == nums[i] {
+        count ++
+      } else {
+        count --
+      }
+    }
+  }
+  return current
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
