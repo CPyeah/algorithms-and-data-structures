0278237b36412dc0637d07c3289fd41cd8e37f7f
chengpeng <chengpengyeah@126.com>
2022-09-22 10:00:56 +0800

    [146] LRU 缓存

--- src/main/java/leetcode/editor/cn/SortList.go
+++ src/main/java/leetcode/editor/cn/SortList.go
@@ -16,7 +16,45 @@
  * }
  */
 func sortList(head *ListNode) *ListNode {
+  if head == nil || head.Next == nil {
+    return head
+  }
+  fast := head
+  slow := head
+  // 这里注意 因为中点取点是slow.Next
+  // 这里需要 && fast.Next.Next != nil
+  for fast != nil && fast.Next != nil && fast.Next.Next != nil {
+    fast = fast.Next.Next
+    slow = slow.Next
+  }
+  right := sortList(slow.Next)
+  slow.Next = nil
+  left := sortList(head)
 
+  return merge2LinkedList(left, right)
+}
+
+func merge2LinkedList(left *ListNode, right *ListNode) *ListNode {
+  dummy := &ListNode{}
+  p := dummy
+  for left != nil && right != nil {
+    if left.Val < right.Val {
+      p.Next = left
+      left = left.Next
+    } else {
+      p.Next = right
+      right = right.Next
+    }
+    p = p.Next
+  }
+  if left != nil {
+    p.Next = left
+  }
+  if right != nil {
+    p.Next = right
+  }
+
+  return dummy.Next
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
