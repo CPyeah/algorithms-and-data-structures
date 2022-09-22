d52a96cca275f6388ab734b69f8926bda682255e
chengpeng <chengpengyeah@126.com>
2022-09-22 14:35:39 +0800

    [200] 岛屿数量

--- src/main/java/leetcode/editor/cn/NumberOfIslands.go
+++ src/main/java/leetcode/editor/cn/NumberOfIslands.go
@@ -9,7 +9,33 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func numIslands(grid [][]byte) int {
-
+  ans := 0
+  rows := len(grid)
+  cols := len(grid[0])
+  // 中心扩散感染，相邻的1都变成0
+  var extend func(i int, j int)
+  extend = func(i int, j int) {
+    if i < 0 || i > rows-1 || j < 0 || j > cols-1 {
+      return
+    }
+    if '1' == grid[i][j] {
+      grid[i][j] = '0'
+      extend(i+1, j)
+      extend(i-1, j)
+      extend(i, j+1)
+      extend(i, j-1)
+    }
+  }
+  // 遍历，遇到的1， 结果++
+  for i:=0;i<rows;i++ {
+    for j:=0;j<cols;j++ {
+      if '1' == grid[i][j] {
+        ans ++
+        extend(i, j)
+      }
+    }
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
