3e45cbf5687cf328cdfed77bda6476f0fa029d7b
chengpeng <chengpengyeah@126.com>
2022-09-22 10:59:36 +0800

    [155] 最小栈

--- src/main/java/leetcode/editor/cn/MinStack.go
+++ src/main/java/leetcode/editor/cn/MinStack.go
@@ -9,32 +9,46 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 type MinStack struct {
-
+  stack []int
+  mStack []int
 }
 
 
 func Constructor() MinStack {
-
+  minStack := MinStack{}
+  minStack.stack = make([]int, 0)
+  minStack.mStack = make([]int, 0)
+  return minStack
 }
 
 
 func (this *MinStack) Push(val int)  {
-
+  this.stack = append(this.stack, val)
+  if len(this.mStack) == 0 || val <= peek(this.mStack) {
+    this.mStack = append(this.mStack, val)
+  }
 }
 
+func peek(s []int) int{
+  return s[len(s)-1]
+}
 
 func (this *MinStack) Pop()  {
-
+  v := peek(this.stack)
+  this.stack = this.stack[:len(this.stack)-1]
+  if v == peek(this.mStack) {
+    this.mStack = this.mStack[:len(this.mStack)-1]
+  }
 }
 
 
 func (this *MinStack) Top() int {
-
+  return peek(this.stack)
 }
 
 
 func (this *MinStack) GetMin() int {
-
+  return peek(this.mStack)
 }
 
 
