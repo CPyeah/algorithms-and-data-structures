3e6efc4fcd72ed5c601bc57dce9814ec96040d16
chengpeng <chengpengyeah@126.com>
2022-09-23 16:38:20 +0800

    [221] 最大正方形

--- src/main/java/leetcode/editor/cn/MaximalSquare.go
+++ src/main/java/leetcode/editor/cn/MaximalSquare.go
@@ -9,7 +9,37 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func maximalSquare(matrix [][]byte) int {
+  dp := make([][]int, len(matrix))
+  ans := 0
+  for i:=0;i<len(matrix);i++ {
+    dp[i] = make([]int, len(matrix[0]))
+    for j:=0;j<len(matrix[0]);j++ {
+      if i == 0 || j == 0 {
+        if matrix[i][j] == '1' {
+          dp[i][j] = 1
+        }
+      } else {
+          if matrix[i][j] == '1' {
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+          }
+      }
+      if dp[i][j] > ans {
+        ans = dp[i][j]
+      }
+    }
+  }
 
+  return ans * ans
+}
+func min(a int, b int, c int) int {
+  m:=a
+  if b < m {
+    m = b
+  }
+  if c < m {
+    m = c
+  }
+  return m
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
