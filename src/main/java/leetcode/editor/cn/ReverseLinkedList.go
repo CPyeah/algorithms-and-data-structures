8e0f2647fd539131fb8cd92e0bfd53324a4b29f5
chengpeng <chengpengyeah@126.com>
2022-09-23 10:21:18 +0800

    [206] 反转链表

--- src/main/java/leetcode/editor/cn/ReverseLinkedList.go
+++ src/main/java/leetcode/editor/cn/ReverseLinkedList.go
@@ -16,7 +16,17 @@
  * }
  */
 func reverseList(head *ListNode) *ListNode {
-
+  if head == nil {
+    return nil
+  }
+  var p1 *ListNode
+  p2, p3 := head, head.Next
+  for p3 != nil {
+    p2.Next = p1
+    p1, p2, p3 = p2, p3, p3.Next
+  }
+  p2.Next = p1
+  return p2
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
