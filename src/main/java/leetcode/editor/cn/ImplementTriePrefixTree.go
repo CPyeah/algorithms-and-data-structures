689a1d267af17d80e8f61203c62528b4ebfd79ee
chengpeng <chengpengyeah@126.com>
2022-09-23 15:15:13 +0800

    [208] 实现 Trie (前缀树)

--- src/main/java/leetcode/editor/cn/ImplementTriePrefixTree.go
+++ src/main/java/leetcode/editor/cn/ImplementTriePrefixTree.go
@@ -9,27 +9,75 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 type Trie struct {
+  Head *Node
+}
 
+type Node struct {
+  Val byte
+  NextNodes []*Node
+  IsEnd bool
 }
 
 
 func Constructor() Trie {
-
+  return Trie{Head: &Node{NextNodes: make([]*Node, 0)}}
 }
 
 
 func (this *Trie) Insert(word string)  {
+  p := this.Head
+  for i:=0; i<len(word); i++ {
+    v := word[i]
+    next := findNext(v, p)
+    if next == nil {
+      next = &Node{Val:v, NextNodes:make([]*Node, 0)}
+      p.NextNodes = append(p.NextNodes, next)
+    }
+    if i == len(word)-1 {
+      next.IsEnd = true
+    }
+    p = next
+  }
+}
 
+func findNext(t byte, p *Node)*Node {
+  if len(p.NextNodes) == 0{
+    return nil
+  }
+  for _,v := range p.NextNodes {
+    if v.Val == t {
+      return v
+    }
+  }
+  return nil
 }
 
 
 func (this *Trie) Search(word string) bool {
-
+  p := this.Head
+  for i:=0; i<len(word); i++ {
+    v := word[i]
+    next := findNext(v, p)
+    if next == nil {
+      return false
+    }
+    p = next
+  }
+  return p.IsEnd
 }
 
 
 func (this *Trie) StartsWith(prefix string) bool {
-
+  p := this.Head
+  for i:=0; i<len(prefix); i++ {
+    v := prefix[i]
+    next := findNext(v, p)
+    if next == nil {
+      return false
+    }
+    p = next
+  }
+  return true
 }
 
 
