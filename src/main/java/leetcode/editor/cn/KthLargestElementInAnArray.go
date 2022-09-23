22d516ccb3083e72f930b461a60fdd18606ba9ff
chengpeng <chengpengyeah@126.com>
2022-09-23 16:27:33 +0800

    [215] 数组中的第K个最大元素

--- src/main/java/leetcode/editor/cn/KthLargestElementInAnArray.go
+++ src/main/java/leetcode/editor/cn/KthLargestElementInAnArray.go
@@ -9,7 +9,32 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func findKthLargest(nums []int, k int) int {
-
+  left, right, t := 0, len(nums)-1, k-1
+  var quickSelect func() int
+  quickSelect = func() int {
+    pivot := nums[right]
+    bigger := left
+    for i:=left;i<right;i++ {
+      if nums[i] > pivot {
+        nums[bigger], nums[i] = nums[i], nums[bigger]
+        bigger ++
+      }
+    }
+    nums[bigger], nums[right] = nums[right], nums[bigger]
+    return bigger
+  }
+  for {
+    index := quickSelect()
+    if index == t {
+      return nums[index]
+    }
+    if index < t {
+      left = index+1
+    } else {
+      right = index-1
+    }
+  }
+  return -1
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
