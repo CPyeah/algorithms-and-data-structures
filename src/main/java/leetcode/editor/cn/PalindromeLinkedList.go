65dff99c07d4e0756bf6ae7e56723d237e44ef7e
chengpeng <chengpengyeah@126.com>
2022-09-23 17:10:46 +0800

    [234] 回文链表

--- src/main/java/leetcode/editor/cn/PalindromeLinkedList.go
+++ src/main/java/leetcode/editor/cn/PalindromeLinkedList.go
@@ -16,7 +16,38 @@
  * }
  */
 func isPalindrome(head *ListNode) bool {
+  // 1、快慢指针，找到中点
+  fast, slow := head, head
+  for fast != nil && fast.Next != nil {
+    fast = fast.Next.Next
+    slow = slow.Next
+  }
+  // 2、反转后半部分链表
+  tail := reverse(slow)
 
+  // 3、比较两个链表的值
+  for head != nil && tail != nil {
+    if head.Val != tail.Val {
+      return false
+    }
+    head = head.Next
+    tail = tail.Next
+  }
+  return true
+}
+
+func reverse(node *ListNode) *ListNode {
+  if node == nil || node.Next == nil {
+    return node
+  }
+  var p1 *ListNode
+  p2, p3 := node, node.Next
+  for p3 != nil {
+    p2.Next = p1
+    p1, p2, p3 = p2, p3, p3.Next
+  }
+  p2.Next = p1
+  return p2
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
