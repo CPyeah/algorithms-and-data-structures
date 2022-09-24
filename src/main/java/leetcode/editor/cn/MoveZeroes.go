168c8de3dbfb7f4a688b8c098a61bf9da5bf4ce2
chengpeng <chengpengyeah@126.com>
2022-09-24 15:12:24 +0800

    [283] 移动零

--- src/main/java/leetcode/editor/cn/MoveZeroes.go
+++ src/main/java/leetcode/editor/cn/MoveZeroes.go
@@ -9,7 +9,13 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func moveZeroes(nums []int)  {
-
+  noZeroIndex := 0
+  for i:=0;i<len(nums);i++ {
+    if nums[i] != 0 {
+      nums[i], nums[noZeroIndex] = nums[noZeroIndex], nums[i]
+      noZeroIndex ++
+    }
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
