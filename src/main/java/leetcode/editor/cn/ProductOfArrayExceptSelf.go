2a7019cb1aff469163843b241748379a4d69a61b
chengpeng <chengpengyeah@126.com>
2022-09-24 11:19:07 +0800

    [238] 除自身以外数组的乘积

--- src/main/java/leetcode/editor/cn/ProductOfArrayExceptSelf.go
+++ src/main/java/leetcode/editor/cn/ProductOfArrayExceptSelf.go
@@ -9,7 +9,21 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func productExceptSelf(nums []int) []int {
-
+  l := len(nums)
+  ans := make([]int, l)
+  // left -> right
+  left := 1
+  for i:=0;i<l;i++ {
+    ans[i] = left
+    left *= nums[i]
+  }
+  // right -> left
+  right := 1
+  for i:=l-1;i>-1;i-- {
+    ans[i] *= right
+    right *= nums[i]
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
