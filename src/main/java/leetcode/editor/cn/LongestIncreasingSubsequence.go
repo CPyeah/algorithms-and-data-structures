6035c68f14c35891a2723ae5dc80f0d68e8f8b0e
chengpeng <chengpengyeah@126.com>
2022-09-24 16:17:57 +0800

    [300] 最长递增子序列

--- src/main/java/leetcode/editor/cn/LongestIncreasingSubsequence.go
+++ src/main/java/leetcode/editor/cn/LongestIncreasingSubsequence.go
@@ -9,8 +9,32 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func lengthOfLIS(nums []int) int {
+  dp := make([]int, len(nums))
+  dp[0] = 1
+  ans := 1
 
+  // 矮子从中挑高个子
+  helper := func(currentIndex int) int {
+    res := 1
+    for i:=0;i<currentIndex;i++ {
+      if nums[i] < nums[currentIndex] {
+        if dp[i] + 1 > res {
+          res = dp[i] + 1
+        }
+      }
+    }
+    return res
+  }
+
+  for i:=1;i<len(nums);i++ {
+    dp[i] = helper(i)
+    if dp[i] > ans {
+      ans = dp[i]
+    }
+  }
+  return ans
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
   }
