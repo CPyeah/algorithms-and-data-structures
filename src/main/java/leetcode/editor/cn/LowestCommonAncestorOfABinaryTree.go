9f52309025e3dccc95aa07569f908e1d7faa5d38
chengpeng <chengpengyeah@126.com>
2022-09-24 14:11:20 +0800

    [236] 二叉树的最近公共祖先

--- src/main/java/leetcode/editor/cn/LowestCommonAncestorOfABinaryTree.go
+++ src/main/java/leetcode/editor/cn/LowestCommonAncestorOfABinaryTree.go
@@ -17,7 +17,28 @@
  * }
  */
  func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-  
+  return dfs(root, p, q)
+}
+
+func dfs(root, p, q *TreeNode) *TreeNode {
+  if root == nil {
+    return nil
+  }
+  if root == p || root == q {
+    return root
+  }
+  left := dfs(root.Left, p, q)
+  right := dfs(root.Right, p, q)
+  if left != nil && right != nil {
+    return root
+  }
+  if left != nil {
+    return left
+  }
+  if right != nil {
+    return right
+  }
+  return nil
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
