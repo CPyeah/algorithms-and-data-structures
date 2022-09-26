6d5d5c60b7749e1977e218741b1876f0292f48d8
chengpeng <chengpengyeah@126.com>
2022-09-26 15:04:18 +0800

    [322] 零钱兑换

--- src/main/java/leetcode/editor/cn/CoinChange.go
+++ src/main/java/leetcode/editor/cn/CoinChange.go
@@ -9,7 +9,28 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func coinChange(coins []int, amount int) int {
-
+  dp := make([]int, amount + 1)
+  dp[0] = 0
+  for i:=1;i<=amount;i++ {
+    dp[i] = -1
+    for _,c := range coins {
+      rest := i - c
+      if rest >= 0 && dp[rest] != -1{
+        if dp[i] == -1 {
+          dp[i] = dp[rest] + 1
+        } else {
+          dp[i] = min(dp[i], dp[rest] + 1)
+        }
+      }
+    }
+  }
+  return dp[amount]
+}
+func min(a int, b int) int {
+  if a<b{
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
