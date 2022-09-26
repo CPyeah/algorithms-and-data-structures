9c6870c7c3d8107f17e55696716d6d983ebd418b
chengpeng <chengpengyeah@126.com>
2022-09-26 15:15:56 +0800

    [338] 比特位计数

--- src/main/java/leetcode/editor/cn/CountingBits.go
+++ src/main/java/leetcode/editor/cn/CountingBits.go
@@ -9,7 +9,12 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func countBits(n int) []int {
-
+  ans := make([]int, n+1)
+  ans[0] = 0
+  for i:=1;i<=n;i++ {
+    ans[i] = ans[i/2] + i%2
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
