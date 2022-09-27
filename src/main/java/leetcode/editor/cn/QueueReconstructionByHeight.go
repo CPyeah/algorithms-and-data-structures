cbda2664aeb014ad3fd01238624a164bced4b6aa
chengpeng <chengpengyeah@126.com>
2022-09-27 15:07:09 +0800

    [406] 根据身高重建队列

--- src/main/java/leetcode/editor/cn/QueueReconstructionByHeight.go
+++ src/main/java/leetcode/editor/cn/QueueReconstructionByHeight.go
@@ -9,7 +9,24 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func reconstructQueue(people [][]int) [][]int {
-
+  sort.Slice(people, func(i, j int)bool {
+    if people[i][0] != people[j][0] {
+      return people[i][0] > people[j][0]
+    }
+      return people[i][1] < people[j][1]
+  })
+  for i:=0;i<len(people);i++ {
+    p := people[i]
+    pos := p[1]
+    if pos < i {
+      // move p to i
+      for j:=i-1;j>=pos;j-- {
+        people[j+1] = people[j]
+      }
+      people[pos] = p
+    }
+  }
+  return people
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
