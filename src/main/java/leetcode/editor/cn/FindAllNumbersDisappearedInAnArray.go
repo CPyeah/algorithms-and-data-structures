7e91262bda8db3ae99ae52e7981038465e752b49
chengpeng <chengpengyeah@126.com>
2022-09-27 14:24:31 +0800

    [448] 找到所有数组中消失的数字

--- src/main/java/leetcode/editor/cn/FindAllNumbersDisappearedInAnArray.go
+++ src/main/java/leetcode/editor/cn/FindAllNumbersDisappearedInAnArray.go
@@ -9,7 +9,28 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func findDisappearedNumbers(nums []int) []int {
+  for i:=0;i<len(nums);i++ {
+    nextIndex := abs(nums[i]) - 1
+    nums[nextIndex] = negative(nums[nextIndex])
+  }
+  ans := make([]int, 0)
+  for i,n := range nums {
+    if n > 0 {
+      ans = append(ans, i+1)
+    }
+  }
+  return ans
+}
 
+func abs(n int) int {
+  if n < 0 {
+    return -n
+  }
+  return n
+}
+
+func negative(n int) int {
+  return -abs(n)
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
