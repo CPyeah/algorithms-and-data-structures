f3fc39a4237b37a568397515d32daf0049242624
chengpeng <chengpengyeah@126.com>
2022-09-29 23:21:38 +0800

    [7] 整数反转

--- src/main/java/leetcode/editor/cn/ReverseInteger.go
+++ src/main/java/leetcode/editor/cn/ReverseInteger.go
@@ -9,7 +9,17 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func reverse(x int) int {
-
+  ans := 0
+  for x != 0 {
+     if ans > math.MaxInt32/10 || ans < math.MinInt32/10 {
+      return 0
+     }
+     ans *= 10
+     yu := x % 10 // 3
+     ans = ans + yu
+     x /= 10 // 12
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
