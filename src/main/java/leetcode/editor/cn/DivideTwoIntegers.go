7e691949c95e51afbba392f8012a7bda35ff71a1
chengpeng <chengpengyeah@126.com>
2022-09-30 10:59:56 +0800

    [29] 两数相除 垃圾题目

--- src/main/java/leetcode/editor/cn/DivideTwoIntegers.go
+++ src/main/java/leetcode/editor/cn/DivideTwoIntegers.go
@@ -9,7 +9,42 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func divide(dividend int, divisor int) int {
+  if dividend == 0 {
+    return 0
+  }
+  ans := 0
+  if divisor == 1 {
+    ans = dividend
+  } else if divisor == -1 {
+    ans = -dividend
+  } else {
+   negative := false
+   if dividend < 0 {
+     negative = !negative
+     dividend = -dividend
+   }
+   if divisor < 0 {
+     negative = !negative
+     divisor = -divisor
+   }
 
+   sum := 0
+   for sum < dividend {
+     sum += divisor
+     ans ++
+   }
+   if sum > dividend {
+     ans --
+   }
+   if negative {
+     ans = -ans
+   }
+  }
+
+  if ans > math.MaxInt32 || ans < math.MinInt32 {
+    return math.MaxInt32
+  }
+  return ans
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
