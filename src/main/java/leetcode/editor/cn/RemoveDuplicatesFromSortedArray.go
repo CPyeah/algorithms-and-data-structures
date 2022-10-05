80f1c766c5d80aff1fb3566c0e3637332d2b1b02
chengpeng <chengpengyeah@126.com>
2022-10-05 14:36:22 +0800

    [26] 删除有序数组中的重复项

--- src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedArray.go
+++ src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedArray.go
@@ -9,7 +9,20 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func removeDuplicates(nums []int) int {
-
+  if len(nums) < 2 {
+    return len(nums)
+  }
+  numIndex := 1
+  // nums[i] 和 nums[i-1]比较
+  // 如果一样，i++
+  // 如果不一样，swap，numIndex++, i++
+  for i:=1;i<len(nums);i++ {
+    if nums[i] != nums[i-1] {
+      nums[numIndex] = nums[i]
+      numIndex ++
+    }
+  }
+  return numIndex
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
