c59cca16b482e8329466af20426753beb7a213ee
chengpeng <chengpengyeah@126.com>
2022-10-07 16:43:50 +0800

    [29] 两数相除

--- src/main/java/leetcode/editor/cn/DivideTwoIntegers.go
+++ src/main/java/leetcode/editor/cn/DivideTwoIntegers.go
@@ -9,39 +9,61 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func divide(dividend int, divisor int) int {
+  // 特殊的边界情况
   if dividend == 0 {
-    return 0
+    return dividend
   }
-  ans := 0
   if divisor == 1 {
-    ans = dividend
-  } else if divisor == -1 {
-    ans = -dividend
-  } else {
-   negative := false
-   if dividend < 0 {
-     negative = !negative
-     dividend = -dividend
-   }
-   if divisor < 0 {
-     negative = !negative
-     divisor = -divisor
-   }
-
-   sum := 0
-   for sum < dividend {
-     sum += divisor
-     ans ++
-   }
-   if sum > dividend {
-     ans --
-   }
-   if negative {
-     ans = -ans
-   }
+    return dividend
+  }
+  if divisor == -1 {
+    dividend = -dividend
+    dividend = limit(dividend)
+    return dividend
   }
 
-  if ans > math.MaxInt32 || ans < math.MinInt32 {
+  // 正负号处理
+  negative := false
+  if dividend < 0 {
+    negative = !negative
+    dividend = -dividend
+  }
+  if divisor < 0 {
+    negative = !negative
+    divisor = -divisor
+  }
+
+  res := 0
+  for dividend >= divisor {
+    time := 1
+    dv := divisor
+    for dv*2 <= dividend {
+      time *= 2
+      dv *= 2
+    }
+    dividend -= dv
+    res += time
+  }
+
+//     for dividend >= divisor {
+//       // 暴力解法：每次减一个dv，记录次数
+//       dividend -= divisor
+//       ans ++
+//     }
+
+  if negative {
+    res = -res
+  }
+
+  // int 边界处理
+  return limit(res)
+}
+
+func limit(ans int) int {
+  if ans < math.MinInt32 {
+    return math.MinInt32
+  }
+  if ans > math.MaxInt32 {
     return math.MaxInt32
   }
   return ans
