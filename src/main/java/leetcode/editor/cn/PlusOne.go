22e8c68e79eac66dc71bd5919d30bf2f0a56402c
chengpeng <chengpengyeah@126.com>
2022-10-10 16:26:32 +0800

    [66] 加一

--- src/main/java/leetcode/editor/cn/PlusOne.go
+++ src/main/java/leetcode/editor/cn/PlusOne.go
@@ -9,7 +9,25 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func plusOne(digits []int) []int {
+  // 先给最后一位加 1
+  digits[len(digits)-1] ++
 
+  // 处理进位问题
+  addOneFlag := false
+  for i:=len(digits)-1;i>=0;i-- {
+    if addOneFlag {
+      digits[i] ++
+      addOneFlag = false
+    }
+    if digits[i] > 9{
+      digits[i] -= 10
+      addOneFlag = true
+    }
+  }
+  if addOneFlag {
+    digits = append([]int{1}, digits...)
+  }
+  return digits
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
