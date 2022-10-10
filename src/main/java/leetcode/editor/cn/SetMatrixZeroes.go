a4a20fb56ca63b6de4e345a50964a5d3bab831d8
chengpeng <chengpengyeah@126.com>
2022-10-10 21:39:52 +0800

    [73] 矩阵置零

--- src/main/java/leetcode/editor/cn/SetMatrixZeroes.go
+++ src/main/java/leetcode/editor/cn/SetMatrixZeroes.go
@@ -9,7 +9,46 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func setZeroes(matrix [][]int)  {
+  // collect all zero
+  firstRow := 1
+  for i:=0;i<len(matrix);i++ {
+    for j:=0;j<len(matrix[0]);j++ {
+      if matrix[i][j] == 0 {
+        matrix[0][j] = 0
+        if i == 0 {
+          firstRow = 0
+        } else {
+          matrix[i][0] = 0
+        }
 
+      }
+    }
+  }
+  setAllRowZero := func(i int) {
+    for j:=0;j<len(matrix[0]);j++ {
+      matrix[i][j] = 0
+    }
+  }
+  setAllColumnZero := func(j int) {
+    for i:=0;i<len(matrix);i++ {
+      matrix[i][j] = 0
+    }
+  }
+  // set zeros
+  // 注意这里的顺序
+  for i:=1;i<len(matrix);i++ {
+    if matrix[i][0] == 0 {
+      setAllRowZero(i)
+    }
+  }
+  for j:=0;j<len(matrix[0]);j++ {
+    if matrix[0][j] == 0 {
+      setAllColumnZero(j)
+    }
+  }
+  if firstRow == 0 {
+    setAllRowZero(0)
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
