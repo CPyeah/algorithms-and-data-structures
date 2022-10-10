c4af800bd233db382369f280b3eda5a2152237cb
chengpeng <chengpengyeah@126.com>
2022-10-10 16:52:51 +0800

    [69] x 的平方根

--- src/main/java/leetcode/editor/cn/Sqrtx.go
+++ src/main/java/leetcode/editor/cn/Sqrtx.go
@@ -9,7 +9,27 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func mySqrt(x int) int {
-
+  if x == 1 {
+    return 1
+  }
+  left, right := 0, x
+  for {
+    mid := (left + right) / 2
+    m2 := mid * mid
+    if m2 == x {
+      return mid
+    }
+    if m2 > x {
+      right = mid
+    } else {
+      if (mid+1) * (mid+1) > x {
+        return mid
+      } else {
+        left = mid
+      }
+    }
+  }
+  return 1
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
