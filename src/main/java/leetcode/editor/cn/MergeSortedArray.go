ad987e8402735317452936a005528a230c82fc19
chengpeng <chengpengyeah@126.com>
2022-10-10 21:57:35 +0800

    [88] 合并两个有序数组

--- src/main/java/leetcode/editor/cn/MergeSortedArray.go
+++ src/main/java/leetcode/editor/cn/MergeSortedArray.go
@@ -9,7 +9,9 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-
+  nums1 = nums1[:m]
+  nums1 = append(nums1, nums2...)
+  sort.Slice(nums1, func(i int, j int)bool {return nums1[i] < nums1[j]})
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
