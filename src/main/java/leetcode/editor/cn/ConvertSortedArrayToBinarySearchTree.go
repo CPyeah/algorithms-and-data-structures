6fc8eb9b4fbff38694370816cc9e6379aa76fa65
chengpeng <chengpengyeah@126.com>
2022-10-12 11:12:47 +0800

    [108] 将有序数组转换为二叉搜索树

--- src/main/java/leetcode/editor/cn/ConvertSortedArrayToBinarySearchTree.go
+++ src/main/java/leetcode/editor/cn/ConvertSortedArrayToBinarySearchTree.go
@@ -17,7 +17,24 @@
  * }
  */
 func sortedArrayToBST(nums []int) *TreeNode {
+  return toTree(nums, 0, len(nums)-1)
+}
 
+func toTree(nums []int, left int, right int) *TreeNode {
+  if left == right {
+    return &TreeNode{Val:nums[left]}
+  }
+  if right - left == 1 {
+    l := &TreeNode{Val:nums[left]}
+    r := &TreeNode{Val:nums[right]}
+    r.Left = l
+    return r
+  }
+  mid := (left + right) / 2
+  m := &TreeNode{Val:nums[mid]}
+  m.Left = toTree(nums, left, mid-1)
+  m.Right = toTree(nums, mid+1, right)
+  return m
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
