2fdb02374100d080f940cd949f3ed17210d477ae
chengpeng <chengpengyeah@126.com>
2022-10-13 14:41:55 +0800

    [118] 杨辉三角

--- src/main/java/leetcode/editor/cn/PascalsTriangle.go
+++ src/main/java/leetcode/editor/cn/PascalsTriangle.go
@@ -9,8 +9,26 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func generate(numRows int) [][]int {
-
+  if numRows == 1 {
+    return [][]int{{1}}
+  }
+  ans := [][]int{{1}}
+  for i:=2;i<=numRows;i++ {
+    newArr := getNew(ans[len(ans)-1])
+    ans = append(ans, newArr)
+  }
+  return ans
 }
+
+func getNew(arr []int) []int {
+  newArr := []int{1}
+  for i:=0;i<len(arr)-1;i++ {
+    newArr = append(newArr, arr[i]+arr[i+1])
+  }
+  newArr = append(newArr, 1)
+  return newArr
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
 
   }
