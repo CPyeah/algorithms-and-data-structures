149c6a547aebf1ae28f46c1719b15747d60a2255
chengpeng <chengpengyeah@126.com>
2022-10-17 22:50:10 +0800

    [171] Excel 表列序号

--- src/main/java/leetcode/editor/cn/ExcelSheetColumnNumber.go
+++ src/main/java/leetcode/editor/cn/ExcelSheetColumnNumber.go
@@ -9,7 +9,28 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func titleToNumber(columnTitle string) int {
+  ans := 0
+  for i:=0;i<len(columnTitle);i++ {
+    b := columnTitle[i]
+    n := getNumber(b) * getPos(len(columnTitle)-i-1)
+    ans += n
+  }
+  return ans
+}
 
+func getPos(time int) int {
+  if time == 0 {
+    return 1
+  }
+  ret := 1
+  for i:=1;i<=time;i++ {
+    ret *= 26
+  }
+  return ret
+}
+
+func getNumber(b byte) int {
+  return int(b) - int('A') + 1
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
