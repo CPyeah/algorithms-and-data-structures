db0cf6661402faaf2ccb45837fa1ce982b151202
chengpeng <chengpengyeah@126.com>
2022-10-19 16:57:53 +0800

    [140] 单词拆分 II

--- src/main/java/leetcode/editor/cn/WordBreakIi.go
+++ src/main/java/leetcode/editor/cn/WordBreakIi.go
@@ -9,7 +9,49 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func wordBreak(s string, wordDict []string) []string {
+  dictMap := make(map[string]bool)
+  maxLen := 0
+  for _,w := range wordDict {
+    dictMap[w] = true
+    if len(w) > maxLen {
+      maxLen = len(w)
+    }
+  }
 
+  result := make([][]string, 0)
+
+  var helper func(reminder string, words []string)
+  helper = func(reminder string, words []string) {
+    if len(reminder) == 0 {
+      result = append(result, words)
+      return
+    }
+    for i:=1;i<=min(maxLen, len(reminder));i++ {
+      w := reminder[:i]
+      if dictMap[w] {
+        newWords := make([]string, len(words))
+        copy(newWords, words)
+        newWords = append(newWords, w)
+        helper(reminder[i:], newWords)
+      }
+    }
+  }
+
+  helper(s, []string{})
+
+  ans := make([]string, len(result))
+  for i:=0;i<len(result);i++ {
+    wordList := result[i]
+    ans[i] = strings.Join(wordList, " ")
+  }
+  return ans
+}
+
+func min(a int, b int) int {
+  if a < b {
+    return a
+  }
+  return b
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
