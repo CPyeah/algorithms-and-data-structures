f060a00244013eaaa6ce4f197b1125c66008ddea
chengpeng <chengpengyeah@126.com>
2022-10-20 22:12:06 +0800

    [149] 直线上最多的点数

--- src/main/java/leetcode/editor/cn/MaxPointsOnALine.go
+++ src/main/java/leetcode/editor/cn/MaxPointsOnALine.go
@@ -9,8 +9,36 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func maxPoints(points [][]int) int {
-
+  if len(points) < 3 {
+    return len(points)
+  }
+  maxCount := 0
+  for i:=0;i<len(points);i++ {
+    currentPoint := points[i]
+    slopeMap := make(map[float64]int)
+    for j:=i+1;j<len(points);j++ {
+      secondPoint := points[j]
+      slope := getSlope(currentPoint, secondPoint)
+      slopeMap[slope]++
+      if slopeMap[slope] > maxCount {
+        maxCount = slopeMap[slope]
+      }
+    }
+  }
+  return maxCount + 1
 }
+
+func getSlope(p1 []int, p2 []int) float64 {
+  x1 := float64(p1[0])
+  y1 := float64(p1[1])
+  x2 := float64(p2[0])
+  y2 := float64(p2[1])
+  if x1 == x2 {
+    return math.MaxFloat64
+  }
+  return (y1-y2)/(x1-x2)
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
 
   }
---
+++ src/main/java/leetcode/editor/cn/RotateArray.go
@@ -0,0 +1,16 @@
+  package leetcode.editor.cn;
+  
+  /**
+   * [189] 轮转数组
+   */
+  public class RotateArray{
+      public static void main(String[] args) {
+           Solution solution = new RotateArray().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+func rotate(nums []int, k int)  {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
