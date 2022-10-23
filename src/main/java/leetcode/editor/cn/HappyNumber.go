165b3a264140fe43e816f224685de423cb9f4d21
chengpeng <chengpengyeah@126.com>
2022-10-23 13:08:00 +0800

    [202] 快乐数

--- src/main/java/leetcode/editor/cn/HappyNumber.go
+++ src/main/java/leetcode/editor/cn/HappyNumber.go
@@ -9,7 +9,25 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func isHappy(n int) bool {
+  visited := make(map[int]bool)
+  for !visited[n] {
+    visited[n] = true
+    n = getHappy(n)
+    if n == 1 {
+      return true
+    }
+  }
+  return false
+}
 
+func getHappy(n int) int {
+  res := 0
+  s := strconv.Itoa(n)
+  for i:=0;i<len(s);i++ {
+    b := int(s[i]-'0')
+    res += b*b
+  }
+  return res
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
