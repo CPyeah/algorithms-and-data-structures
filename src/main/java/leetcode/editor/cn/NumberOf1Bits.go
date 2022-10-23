6e9844e8942bea00fbcc51aff9f8616cf15d7f86
chengpeng <chengpengyeah@126.com>
2022-10-23 12:33:56 +0800

    [191] 位1的个数

--- src/main/java/leetcode/editor/cn/NumberOf1Bits.go
+++ src/main/java/leetcode/editor/cn/NumberOf1Bits.go
@@ -9,7 +9,16 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func hammingWeight(num uint32) int {
-    
+    count := 0
+    for i:=0;i<32;i++ {
+      // 看最后一位是不是 1
+      if num & 1 == 1 {
+        count ++
+      }
+      // 右移 1 位
+      num >>= 1
+    }
+    return count
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
