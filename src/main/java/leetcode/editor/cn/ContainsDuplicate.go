f4778f14bd801e718569f5015ed6b8526ab69095
chengpeng <chengpengyeah@126.com>
2022-10-23 17:02:46 +0800

    [217] 存在重复元素

--- src/main/java/leetcode/editor/cn/ContainsDuplicate.go
+++ src/main/java/leetcode/editor/cn/ContainsDuplicate.go
@@ -9,7 +9,15 @@
       }
       //leetcode submit region begin(Prohibit modification and deletion)
 func containsDuplicate(nums []int) bool {
-
+  cache := make(map[int]bool)
+  for _,n := range nums {
+    if cache[n] {
+      return true
+    } else {
+      cache[n] = true
+    }
+  }
+  return false
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
